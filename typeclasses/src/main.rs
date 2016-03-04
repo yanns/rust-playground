92e9e9bd25699e2431acfc22634fd56584ba9a2b
Yann Simon <yann.simon.fr@gmail.com>
2016-03-04 21:09:35 +0100

    experiment with type classes

---
+++ typeclasses/src/main.rs
@@ -0,0 +1,71 @@
+trait ToJson {
+	fn to_json(&self) -> String;
+}
+
+impl ToJson for str {
+    fn to_json(&self) -> String {
+    	format!("\"{}\"", &self)
+    }
+}
+
+impl ToJson for String {
+    fn to_json(&self) -> String {
+    	format!("\"{}\"", &self)
+    }
+}
+
+impl ToJson for i32 {
+    fn to_json(&self) -> String {
+    	format!("{}", &self)
+    }	
+}
+
+impl<T: ToJson> ToJson for Vec<T> {
+    fn to_json(&self) -> String {
+		let mut result = String::new();
+		result = result + "[";
+		let mut first = true;
+		for i in self {
+			result = if first {
+				first = false;
+				result + &i.to_json()
+			} else {
+				result + &", ".to_string() + &i.to_json()
+			}
+		}
+		result + "]"
+    }		
+}
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: i32,
+}
+impl Person {
+	fn new(name: &str, age: i32) -> Person {
+		Person { name: name.to_string(), age: age }
+	}
+}
+
+impl ToJson for Person {
+    fn to_json(&self) -> String {
+    	format!(r#"{{
+  "name": {},
+  "age": {}
+}}"#, self.name.to_json(), self.age.to_json())
+    }	
+}
+
+fn main() {
+    println!("{}", "hello world".to_json());
+    println!("{}", 3.to_json());
+    println!("{}", vec![1, 2, 3, 4, 5].to_json());
+    println!("{}", vec!["hello".to_string(), "world".to_string()].to_json());
+    // does not compile
+    // println!("{}", vec!["hello", "world"].to_json());
+
+    let p1 = Person::new("yann", 38);
+    println!("{}", p1.to_json());
+    println!("{}", vec![p1, Person::new("paul", 58)].to_json());
+}

93dcc2e2e0fc09d6760704f4f3586fae6ea6bc7c
Yann Simon <yann.simon.fr@gmail.com>
2016-08-03 18:30:42 +0200

    impl together

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -11,6 +11,17 @@ struct Node<T> {
     next: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
+pub struct IterMut<'a, T: 'a> {
+    next: Option<&'a mut Node<T>>,
+}
+
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -40,6 +51,18 @@ impl<T> List<T> {
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| &mut node.elem)
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut { next: self.head.as_mut().map(|node| &mut **node) }
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -55,28 +78,10 @@ impl<T> Drop for List<T> {
 }
 
 
-pub struct IntoIter<T>(List<T>);
-
-impl<T> List<T> {
-    pub fn into_iter(self) -> IntoIter<T> {
-        IntoIter(self)
-    }
-}
-
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop()
-    }
-}
-
-pub struct Iter<'a, T: 'a> {
-    next: Option<&'a Node<T>>,
-}
-
-impl<T> List<T> {
-    pub fn iter(&self) -> Iter<T> {
-        Iter { next: self.head.as_ref().map(|node| &**node) }
     }
 }
 
@@ -88,16 +93,6 @@ impl<'a, T> Iterator for Iter<'a, T> {
             self.next = node.next.as_ref().map(|node| &**node);
             &node.elem
         })
-    }
-}
-
-pub struct IterMut<'a, T: 'a> {
-    next: Option<&'a mut Node<T>>,
-}
-
-impl<T> List<T> {
-    pub fn iter_mut(&mut self) -> IterMut<T> {
-        IterMut { next: self.head.as_mut().map(|node| &mut **node) }
     }
 }
 

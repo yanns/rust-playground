73a908e38736a5d8606660c0422715088d532db8
Yann Simon <yann.simon.fr@gmail.com>
2016-10-13 21:19:07 +0200

    draw a rectangle

--- life/src/main.rs
+++ life/src/main.rs
@@ -1,13 +1,19 @@
 extern crate sdl2;
+extern crate rand;
+
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 
 use std::{thread, time};
-use sdl2::pixels::Color;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::Renderer;
+use sdl2::EventPump;
 
-fn main() {
+fn init<'a>() -> (Renderer<'a>, EventPump) {
     let sdl_context = sdl2::init().unwrap();
+
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem.window("demo", 400, 400)
@@ -17,10 +23,20 @@ fn main() {
         .unwrap();
 
     let mut renderer = window.renderer().build().unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let event_pump = sdl_context.event_pump().unwrap();
+
+    renderer.set_draw_color(Color::RGB(255, 255, 255));
+    renderer.clear();
+    renderer.present();
+
+    (renderer, event_pump)
+}
+
+fn main() {
+    let (mut renderer, mut event_pump) = init();
 
     renderer.set_draw_color(Color::RGB(255, 0, 0));
-    renderer.clear();
+    renderer.fill_rect(Rect::new(10, 10, 100, 100)).unwrap();
     renderer.present();
 
     'running: loop {

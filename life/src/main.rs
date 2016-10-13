a784c99c1b2070bbf11b19be01d36dd947583fad
Yann Simon <yann.simon.fr@gmail.com>
2016-10-13 21:13:42 +0200

    exit on escape

--- life/src/main.rs
+++ life/src/main.rs
@@ -3,6 +3,9 @@ extern crate sdl2;
 use std::{thread, time};
 use sdl2::pixels::Color;
 
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -14,9 +17,19 @@ fn main() {
         .unwrap();
 
     let mut renderer = window.renderer().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
     renderer.set_draw_color(Color::RGB(255, 0, 0));
     renderer.clear();
     renderer.present();
 
-    thread::sleep(time::Duration::from_millis(2000));
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
+                _ => {}
+            }
+        }
+        thread::sleep(time::Duration::from_millis(10));
+    }
 }

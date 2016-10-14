60fffa0d0cbd7fc9435ce694d765dc18a44307a5
Yann Simon <yann.simon.fr@gmail.com>
2016-10-14 11:46:33 +0200

    hide Vec from main

--- life/src/main.rs
+++ life/src/main.rs
@@ -18,38 +18,145 @@ const CELL_WIDTH: u32 = 1;
 const CELL_HEIGHT: u32 = CELL_WIDTH;
 const NCELLS: u32 = (MAX_X + 1) / CELL_WIDTH;
 
-fn life_random(ncells: u32) -> Vec<Vec<bool>> {
-    let mut rng = rand::thread_rng();
-
-    let mut v: Vec<Vec<bool>> = Vec::new();
-
-    for i in 0..ncells {
-        v.push(Vec::new());
-        for _ in 0..ncells {
-            v[i as usize].push(rng.gen());
-        }
-    }
-
-    v
+struct Grid {
+    cells: Vec<Vec<bool>>,
 }
 
-fn glider(ncells: u32) -> Vec<Vec<bool>> {
-    let mut v: Vec<Vec<bool>> = Vec::with_capacity(NCELLS as usize);
-
-    for i in 0..ncells {
-        v.push(Vec::with_capacity(NCELLS as usize));
-        for _ in 0..ncells {
-            v[i as usize].push(false);
+impl Grid {
+    fn new() -> Grid {
+        let mut v: Vec<Vec<bool>> = Vec::with_capacity(NCELLS as usize);
+        for i in 0..NCELLS {
+            v.push(Vec::with_capacity(NCELLS as usize));
+            for _ in 0..NCELLS {
+                v[i as usize].push(false);
+            }
+        }
+        Grid {
+            cells: v,
         }
     }
 
-    v[10][11] = true;
-    v[11][12] = true;
-    v[12][10] = true;
-    v[12][11] = true;
-    v[12][12] = true;
+    fn life_random() -> Grid {
+        let mut rng = rand::thread_rng();
+        let mut v: Vec<Vec<bool>> = Vec::with_capacity(NCELLS as usize);
 
-    v
+        for i in 0..NCELLS {
+            v.push(Vec::with_capacity(NCELLS as usize));
+            for _ in 0..NCELLS {
+                v[i as usize].push(rng.gen());
+            }
+        }
+
+        Grid {
+            cells: v,
+        }
+    }
+
+    fn glider(ncells: u32) -> Grid {
+        let mut v: Vec<Vec<bool>> = Vec::with_capacity(NCELLS as usize);
+
+        for i in 0..ncells {
+            v.push(Vec::with_capacity(NCELLS as usize));
+            for _ in 0..ncells {
+                v[i as usize].push(false);
+            }
+        }
+
+        v[10][11] = true;
+        v[11][12] = true;
+        v[12][10] = true;
+        v[12][11] = true;
+        v[12][12] = true;
+
+        Grid {
+            cells: v,
+        }
+    }
+
+    fn set(&mut self, r: usize, c: usize, value: bool) {
+        self.cells[r][c] = value;
+    }
+
+    fn get(&self, r: usize, c: usize) -> bool {
+        self.cells[r][c]
+    }
+
+    fn alive(&self, r: u32, c: u32) -> bool {
+        let n = self.count_surrounding(r, c);
+
+        let curr = self.cells[r as usize][c as usize];
+
+        match (curr, n) {
+            (true, 0...1) => false,
+            (true, 2...3) => true,
+            (true, _) => false,
+            (false, 0...2) => false,
+            (false, 3) => true,
+            (false, _) => false,
+        }
+    }
+
+    #[cfg_attr(rustfmt, rustfmt_skip)]
+    fn count_surrounding(&self, r: u32, c: u32) -> i32 {
+        let r = r as usize;
+        let c = c as usize;
+        let dec_r = dec(r);
+        let inc_r = inc(r);
+        let dec_c = dec(c);
+        let inc_c = inc(c);
+        let v_r = &self.cells[r];
+        let v_dec_r = &self.cells[dec_r];
+        let v_inc_r = &self.cells[inc_r];
+
+        v_dec_r[c] as i32 +
+        v_inc_r[c] as i32 +
+        v_r[dec_c] as i32 +
+        v_r[inc_c] as i32 +
+        v_dec_r[dec_c] as i32 +
+        v_dec_r[inc_c] as i32 +
+        v_inc_r[inc_c] as i32 +
+        v_inc_r[dec_c] as i32
+    }
+}
+
+trait DisplayFrame {
+    fn display_frame(&self, r: &mut Renderer);
+}
+
+impl DisplayFrame for Grid {
+    fn display_frame(&self, r: &mut Renderer) {
+        r.set_draw_color(Color::RGB(200, 200, 200));
+        r.clear();
+
+        for i in 0..NCELLS {
+            for j in 0..NCELLS {
+                if self.get(i as usize, j as usize) {
+                    display_cell(r, i, j);
+                }
+            }
+        }
+
+        r.present();
+    }
+}
+
+trait LifeNext {
+    fn life_next(&self) -> Self;
+}
+
+impl LifeNext for Grid {
+    fn life_next(&self) -> Self {
+        let mut grid2 = Grid::new();
+
+        for i in 0..NCELLS {
+            for j in 0..NCELLS {
+                let a = self.alive(i, j);
+                grid2.set(i as usize, j as usize, a);
+            }
+        }
+
+        grid2
+    }
 }
 
 fn display_cell(r: &mut Renderer, col: u32, row: u32) {
@@ -59,58 +166,6 @@ fn display_cell(r: &mut Renderer, col: u32, row: u32) {
     let cell_color = Color::RGB(255, 0, 0);
     r.set_draw_color(cell_color);
     r.fill_rect(Rect::new(x as i32, y as i32, CELL_WIDTH, CELL_HEIGHT)).unwrap();
-}
-
-fn display_frame(r: &mut Renderer, v: &Vec<Vec<bool>>) {
-    r.set_draw_color(Color::RGB(200, 200, 200));
-    r.clear();
-
-    for i in 0..NCELLS {
-        for j in 0..NCELLS {
-            if v[i as usize][j as usize] {
-                display_cell(r, i, j);
-            }
-        }
-    }
-
-    r.present();
-}
-
-fn alive(r: u32, c: u32, v: &Vec<Vec<bool>>) -> bool {
-    let n = count_surrounding(r, c, v);
-
-    let curr = v[r as usize][c as usize];
-
-    match (curr, n) {
-        (true, 0...1) => false,
-        (true, 2...3) => true,
-        (true, _) => false,
-        (false, 0...2) => false,
-        (false, 3) => true,
-        (false, _) => false,
-    }
-}
-
-#[cfg_attr(rustfmt, rustfmt_skip)]
-fn count_surrounding(r: u32, c: u32, v: &Vec<Vec<bool>>) -> i32 {
-    let r = r as usize;
-    let c = c as usize;
-    let dec_r = dec(r);
-    let inc_r = inc(r);
-    let dec_c = dec(c);
-    let inc_c = inc(c);
-    let v_r = &v[r];
-    let v_dec_r = &v[dec_r];
-    let v_inc_r = &v[inc_r];
-
-    v_dec_r[c] as i32 +
-    v_inc_r[c] as i32 +
-    v_r[dec_c] as i32 +
-    v_r[inc_c] as i32 +
-    v_dec_r[dec_c] as i32 +
-    v_dec_r[inc_c] as i32 +
-    v_inc_r[inc_c] as i32 +
-    v_inc_r[dec_c] as i32
 }
 
 fn inc(n: usize) -> usize {
@@ -124,21 +179,6 @@ fn dec(n: usize) -> usize {
         (n - 1) as usize
     }
 }
-
-fn life_next(v: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
-    let mut v2: Vec<Vec<bool>> = Vec::with_capacity(NCELLS as usize);
-
-    for i in 0..NCELLS {
-        v2.push(Vec::with_capacity(NCELLS as usize));
-        for j in 0..NCELLS {
-            let a = alive(i, j, &v);
-            v2[i as usize].push(a);
-        }
-    }
-
-    v2
-}
-
 
 fn init<'a>() -> (Renderer<'a>, EventPump) {
     let sdl_context = sdl2::init().unwrap();
@@ -164,7 +204,7 @@ fn init<'a>() -> (Renderer<'a>, EventPump) {
 fn main() {
     let (mut renderer, mut event_pump) = init();
 
-    let mut v = life_random(NCELLS);
+    let mut grid = Grid::life_random();
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -173,8 +213,8 @@ fn main() {
                 _ => {}
             }
         }
-        display_frame(&mut renderer, &v);
-        v = life_next(v);
+        grid.display_frame(&mut renderer);
+        grid = grid.life_next();
         thread::sleep(time::Duration::from_millis(50));
     }
 }

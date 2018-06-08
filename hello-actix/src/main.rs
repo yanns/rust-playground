fd0658db21c5c80776afff534cb15cd951fd9452
Yann Simon <yann.simon.fr@gmail.com>
2018-06-08 10:19:02 +0200

    app with state

--- hello-actix/src/main.rs
+++ hello-actix/src/main.rs
@@ -1,12 +1,28 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest};
 
-fn index(_req: HttpRequest) -> &'static str {
-    "Hello world!"
+use actix_web::{http, server, App, HttpRequest};
+use std::cell::Cell;
+
+// This struct represents state
+struct AppState {
+    counter: Cell<usize>,
+}
+
+fn index(req: HttpRequest<AppState>) -> String {
+    let count = req.state().counter.get() + 1;
+    req.state().counter.set(count);
+
+    format!("Request number: {}", count)
+}
+
+fn new_app() -> App<AppState> {
+    App::with_state(AppState {
+        counter: Cell::new(0),
+    }).resource("/", |r| r.method(http::Method::GET).f(index))
 }
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
+    server::new(|| new_app())
         .bind("127.0.0.1:8088")
         .unwrap()
         .run();
